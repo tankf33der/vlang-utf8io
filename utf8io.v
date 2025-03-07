2a472e2dbb828777222ace3710c1a4a45bf330f5
Mike <tankf33der@disroot.org>
2025-03-07 13:00:19 +0200

    .

--- utf8io.v
+++ utf8io.v
@@ -1,12 +1,25 @@
-module utf8io
+// module utf8io
+
+import os
 
 pub struct Utf8io{
 pub mut:
-	f	File
+	f	os.File
 }
 
-pub open(path string)! {
-
+pub fn (u Utf8io) open(path string) !os.File {
+	return os.open(path)!
 
 }
 
+pub fn (mut u Utf8io) close() {
+	u.f.close()
+}
+
+fn main() {
+	mut u := Utf8io{}
+	u.f = u.open('../m.dat')!
+	println(u.f.read_u8()!)
+
+	u.close()
+}
