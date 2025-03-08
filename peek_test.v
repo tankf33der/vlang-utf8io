caf51cd91c540f8bea62ef4bab7fd712b5b86a83
Mike <tankf33der@disroot.org>
2025-03-08 16:27:33 +0200

    .

---
+++ peek_test.v
@@ -0,0 +1,5 @@
+module utf8io
+
+fn test_peek() {
+
+}
--- utf8io.v
+++ utf8io.v
@@ -1,4 +1,5 @@
-// module utf8io
+module utf8io
+
 import os
 
 pub struct Utf8io {
@@ -12,11 +13,11 @@ pub fn (mut u Utf8io) open(path string) {
 }
 
 pub fn (mut u Utf8io) peek_char() ![]u8 {
-	mut res := []u8{}
-	res << u.f.read_u8() or { return res }
+	mut res := []u8{cap: 4}
+	res << u.f.read_u8()!
 	count := utf8_char_len(res[0])
 	for _ in 0 .. count - 1 {
-		res << u.f.read_u8()!
+		res <<  u.f.read_u8()!
 	}
 	defer {
 		u.f.seek(u.pos, .start) or { panic('seek failed') }
@@ -24,9 +25,10 @@ pub fn (mut u Utf8io) peek_char() ![]u8 {
 	return res
 }
 
-pub fn (mut u Utf8io) read_char() ![]u8 {
-	mut res := []u8{}
-	res << u.f.read_u8() or { return res }
+/*
+pub fn (mut u Utf8io) read_char() ?[]u8 {
+	mut res := []u8{cap: 4}
+	res << u.f.read_u8() or { return none }
 	count := u64(utf8_char_len(res[0]))
 	u.pos += count
 	for _ in 0 .. count - 1 {
@@ -34,21 +36,29 @@ pub fn (mut u Utf8io) read_char() ![]u8 {
 	}
 	return res
 }
+*/
+
+/*
+pub fn (mut u Utf8io) read_line() ![]u8 {
+	mut res := []u8{}
+	for {
+		ch := u.read_char() or { break }
+		println(ch)
+		if ch.len == 0 || ch[0] == 10 || ch[0] == 13 {
+			break
+		}
+		res << ch
+	}
+	return res
+}
+*/
+
 
 pub fn (mut u Utf8io) close() {
 	u.f.close()
 }
 
-fn main() {
-	mut u := Utf8io{}
-	u.open('../m2.dat')
-	defer { u.close() }
-
-	for {
-		c := u.read_char()!
-		if c.len == 0 || c[0] == 10 {
-			break
-		}
-		println('read:|${c}|')
-	}
+@[unsafe]
+pub fn (mut u Utf8io) free() {
+	unsafe { free(u) }
 }
