d6b3af750a48ad1cf628845cd07ad8f7762f52ba
Mike <tankf33der@disroot.org>
2025-03-08 16:33:50 +0200

    .

--- peek_test.v
+++ peek_test.v
@@ -2,8 +2,8 @@ module utf8io
 
 fn test_peek() {
 	mut u := utf8io.Utf8io{}
-	u.open('./testdata/EOF.dat')
-	println(u.peek_char()!)
+	u.open('./testdata/zero.dat')
+	println(u.read_char()!)
 	u.close()
 	unsafe { u.free() }
 }
