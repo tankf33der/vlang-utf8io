547a521f9abf737af95e92c7686256ee1595a91b
Mike <tankf33der@disroot.org>
2025-03-08 15:53:29 +0200

    .

--- utf8io.v
+++ utf8io.v
@@ -11,9 +11,9 @@ pub fn (mut u Utf8io) open(path string) {
 	u.f = os.open(path) or { panic('open failed') }
 }
 
-pub fn (mut u Utf8io) peek_char() !string {
+pub fn (mut u Utf8io) peek_char() ![]u8 {
 	mut res := []u8{}
-	res << u.f.read_u8() or { return res.bytestr() }
+	res << u.f.read_u8() or { return res }
 	count := utf8_char_len(res[0])
 	for _ in 0 .. count - 1 {
 		res << u.f.read_u8()!
@@ -21,18 +21,18 @@ pub fn (mut u Utf8io) peek_char() !string {
 	defer {
 		u.f.seek(u.pos, .start) or { panic('seek failed') }
 	}
-	return res.bytestr()
+	return res
 }
 
-pub fn (mut u Utf8io) read_char() !string {
+pub fn (mut u Utf8io) read_char() ![]u8 {
 	mut res := []u8{}
-	res << u.f.read_u8() or { return res.bytestr() }
+	res << u.f.read_u8() or { return res }
 	count := u64(utf8_char_len(res[0]))
 	u.pos += count
 	for _ in 0 .. count - 1 {
 		res << u.f.read_u8()!
 	}
-	return res.bytestr()
+	return res
 }
 
 pub fn (mut u Utf8io) close() {
@@ -41,23 +41,14 @@ pub fn (mut u Utf8io) close() {
 
 fn main() {
 	mut u := Utf8io{}
-	// mut res := []u8{cap: 4}
 	u.open('../m2.dat')
 	defer { u.close() }
 
-	mut c := ''
 	for {
-		// println('start')
-		// c = u.peek_char()!
-		// if c.len == 0 { break }
-		// println('peek:|${c}|')
-
-		c = u.read_char()!
-		if c.len == 0 {
+		c := u.read_char()!
+		if c.len == 0 || c[0] == 10 {
 			break
 		}
-		fl := c.is_blank()
-		println('read:|${c}|${fl}')
-		// println('end')
+		println('read:|${c}|')
 	}
 }
