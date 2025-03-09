3d1baf2be41303228cfcecccb8f7e993a6fc6569
Mike <tankf33der@disroot.org>
2025-03-09 21:49:24 +0200

    .

--- read_from_test.v
+++ read_from_test.v
@@ -30,5 +30,14 @@ fn test_read_from2_1() {
 	u.open('./testdata/readfrom2.dat')
 	res := u.read_from('vlang')!
 	assert res.bytestr() == 'vlang'
+	assert u.read_char()!.bytestr() == 'b'
+	u.close()
+}
+
+fn test_read_from3_1() {
+	mut u := Utf8io{}
+	u.open('./testdata/readfrom3.dat')
+	res := u.read_from(':')!
+	assert res.bytestr() == ':'
 	u.close()
 }
