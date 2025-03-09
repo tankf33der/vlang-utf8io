c48d175079e4c657e8081bf19f52159f9d915a23
Mike <tankf33der@disroot.org>
2025-03-09 11:25:49 +0200

    .

---
+++ read_till_test.v
@@ -0,0 +1,13 @@
+module utf8io
+
+fn test_read_till1() {
+	mut u := utf8io.Utf8io{}
+	u.open('./testdata/readtill1.dat')
+
+	res := u.read_till(':')!
+	println(res)
+	println(peek_char()!)
+
+
+	u.close()
+}
